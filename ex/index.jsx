a109d805a66d5743b4151c3e2eaf1cdefb63b0c6
Cristian Araujo <araujocristian@live.com>
2018-05-18 20:21:16 -0300

    create contador

--- ex/index.jsx
+++ ex/index.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom'
 import ComponentClass from "./componentClass";
 
 ReactDOM.render(
-    <ComponentClass value="Componente Classe" />
+    <ComponentClass label='Contador' initialValue={10} />
 , document.getElementById('app'))
