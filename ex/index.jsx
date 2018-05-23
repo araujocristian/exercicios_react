84cdd4654774370113f0d278661dad1bd5d4361b
Cristian Araujo <araujocristian@live.com>
2018-05-23 14:46:02 -0300

    add reducers

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 import Field from "./field";
 
+const reducers = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
+
 ReactDOM.render(
-    <Field initialValue="Text" />
+    <Provider store={createStore(reducers)}>
+        <Field initialValue="Text" />
+    </Provider>
 , document.getElementById('app'))
